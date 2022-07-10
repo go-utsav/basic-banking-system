b95b31802fa83f878bdfba3b47389a291d22b011
Utsav Gohel <69647093+go-utsav@users.noreply.github.com>
2022-07-10 10:12:50 +0530

    Add files via upload

---
+++ navigationbar.php
@@ -0,0 +1,37 @@
+
+    <!--Navigation-->
+    <nav class="navbar navbar-expand-lg " style="background-color: #07c988;">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand " href="index.html"></a><img style=" width: 100px;
+    height: 80px;" src="img/bank.png" alt="" srcset=""><h1>SM Finances</h1></a>
+        
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <b>
+                  <ul class="navbar-nav ">
+                      <li class="nav-item nav-links" style="margin-left:15px;">
+                          <a class="nav-link active text-nowrap" aria-current="page" href="index.html">Home</a>
+                      </li>
+                      <li class="nav-item" style="margin-left:15px; ">
+                          <a class="nav-link text-nowrap" href="transfercoin.php">Send Coin</a>
+                      </li>
+                      <li class="nav-item" style="margin-left:15px;">
+                          <a class="nav-link text-nowrap" href="viewcustomer.php">View All Customers</a>
+                      </li>
+                      <li class="nav-item" style="margin-left:15px;">
+                          <a class="nav-link text-nowrap" href="transaction.php">Transactions</a>
+                      </li>
+                      
+                      <li class="nav-item" style="margin-left:15px; margin-right:75px;">
+                          <a class="nav-link text-nowrap" href="about_us.php">About Us</a>
+                      </li>
+                  </ul>
+              </b>
+          </div>
+      
+          </div>
+        </div>
+      </nav>
+      <!--Navigation Closed-->
